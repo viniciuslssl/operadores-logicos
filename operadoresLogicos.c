ce90ed32489cb29913e32e24270c22569e4e11ad
viniciuslssl <142353095+viniciuslssl@users.noreply.github.com>
2023-08-29 19:37:02 -0300

    Add files via upload

---
+++ operadoresLogicos.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+
+
+main(){
+
+
+
+int media = 7;
+int mediaAluno;
+
+printf("Digite sua nota");
+scanf("%d",&mediaAluno);
+
+if(mediaAluno >= media){
+    Printf("Aprovado");
+}else if (mediaAluno < media){
+    printf("Recuperacao");
+}else {printf("reprovado");}
+
+
+
+
+
+}
